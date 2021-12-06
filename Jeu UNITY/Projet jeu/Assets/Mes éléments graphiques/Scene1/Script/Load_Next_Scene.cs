727d1ccaa8f89f4d6effcbc81f11a716a816e2b2
MarieMoalla <86363907+MarieMoalla@users.noreply.github.com>
2021-12-06 18:53:08 +0100

    Mariem first update

---
+++ Jeu UNITY/Projet jeu/Assets/Mes éléments graphiques/Scene1/Script/Load_Next_Scene.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Next_Scene : MonoBehaviour
+{
+    
+    public void Load_Scene(string name)
+    {
+         SceneManager.LoadScene(name);
+    }
+    public void exit(){
+        Application.Quit();
+    }
+
+}
---
+++ Jeu UNITY/Projet jeu/Assets/Mes éléments graphiques/Scene1/Script/Popups.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Popups : MonoBehaviour
+{
+    public GameObject GO;
+
+    public void openPopUp(){
+        GO.SetActive(true);
+    }
+        public void closePopUp(){
+        GO.SetActive(false);
+    }
+}
