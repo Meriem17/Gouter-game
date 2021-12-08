b16d53f2ddebb90cf6dc556016dbbf3491224b7e
MarieMoalla <86363907+MarieMoalla@users.noreply.github.com>
2021-12-08 11:36:43 +0100

    add_rules_interface

--- Jeu UNITY/Projet jeu/Assets/Mes éléments graphiques/Scene1/Script/Popups.cs
+++ Jeu UNITY/Projet jeu/Assets/Mes éléments graphiques/Scene1/Script/Popups.cs
@@ -6,11 +6,12 @@ using UnityEngine.UI;
 public class Popups : MonoBehaviour
 {
     public GameObject GO;
-
     public void openPopUp(){
+        Debug.Log("open");
         GO.SetActive(true);
     }
         public void closePopUp(){
+        Debug.Log("close");
         GO.SetActive(false);
     }
 }
