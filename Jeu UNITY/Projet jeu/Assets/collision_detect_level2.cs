60525f5f1ee268d7af2a894ef9b1cccdf3b9312f
mariem <86363907+MarieMoalla@users.noreply.github.com>
2022-01-03 13:38:05 +0100

    First step with level2

---
+++ Jeu UNITY/Projet jeu/Assets/collision_detect_level2.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+using TMPro;
+
+public class collision_detect_level2 : MonoBehaviour
+{
+    public AudioSource hum;
+
+    public TextMeshProUGUI kakiScore;
+    private int kakiNb;
+
+    public TextMeshProUGUI creamScore;
+    private int creamNb;
+
+
+    void Start()
+    {
+        kakiNb = 0;
+        kakiScore.text = kakiNb.ToString();
+
+        creamNb = 0;
+        creamScore.text = creamNb.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     #region On Trigger Enter
+    private void OnTriggerEnter2D (Collider2D col) {
+        
+        if (col.gameObject.tag == "Vitamin" ) {
+            
+            hum.Play();
+            col.gameObject.active = false;
+            //Incriment(0.1);
+            if(col.gameObject.name.Contains("kaki"))
+            {
+                kakiNb++;
+                kakiScore.text = kakiNb.ToString();
+            }
+            if(col.gameObject.name.Contains("crème"))
+            {
+                creamNb++;
+                creamScore.text = creamNb.ToString();
+            }
+           
+        }
+    }
+    #endregion
+}
---
+++ Jeu UNITY/Projet jeu/Assets/player_controle_level2.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controle_level2 : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+
+    private bool isJumping;
+    private float move;
+    private Rigidbody2D rb;
+    private Animator anim;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if(move < 0) transform.eulerAngles = new Vector3(0, 180, 0);
+        else if(move > 0) transform.eulerAngles = new Vector3 (0,0,0);
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpSpeed));
+            isJumping = true;
+        }
+        RunAnimation();
+    }
+
+    private void OnCollisionEnter2D (Collision2D col)
+    {
+        if(col.gameObject.CompareTag("Ground")) 
+        {
+          isJumping = false;
+        }
+        
+    }
+
+    void RunAnimation()
+    {
+        Debug.Log("running");
+        anim.SetFloat("Movement", Mathf.Abs(move));
+        anim.SetBool("Jumping", isJumping);
+    }
+}
